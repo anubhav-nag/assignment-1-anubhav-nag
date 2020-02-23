c5f7c1be2412582af647f497a1c2b7026275fb98
anubhav.nag_cs18 <anubhav.nag_cs18@gla.ac.in>
2020-02-23 22:30:52 +0530

    creating constructor and getter for Node class

--- src/node/Node.java
+++ src/node/Node.java
@@ -4,5 +4,16 @@ public class Node<E> {
     private E data;
     private Node next;
 
+    public Node(E data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 
+    public E getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
 }
