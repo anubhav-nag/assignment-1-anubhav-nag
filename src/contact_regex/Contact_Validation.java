c58c07fa03a55180632068a2fdc247d9fd674098
anubhav.nag_cs18 <anubhav.nag_cs18@gla.ac.in>
2020-02-23 23:26:45 +0530

    add methods for validation

--- src/contact_regex/Contact_Validation.java
+++ src/contact_regex/Contact_Validation.java
@@ -1,4 +1,20 @@
 package contact_regex;
 
 public class Contact_Validation {
+
+    public boolean checkFirstName() {
+        return true;
+    }
+
+    public boolean checkLasttName() {
+        return true;
+    }
+
+    public boolean checkEamil() {
+        return true;
+    }
+
+    public boolean checkContactNumber() {
+        return true;
+    }
 }
