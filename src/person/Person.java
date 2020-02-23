e5a67a122dc5531e09bfa6545901d31c01863fe5
anubhav.nag_cs18 <anubhav.nag_cs18@gla.ac.in>
2020-02-23 22:42:22 +0530

    create constructor and getter methods

--- src/person/Person.java
+++ src/person/Person.java
@@ -6,4 +6,26 @@ public class Person {
     private String emailAddress;
     private StringBuilder contactNumbers;
 
+    public Person(String firstName, String lastName, String emailAddress, StringBuilder contactNumbers) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.contactNumbers = contactNumbers;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public StringBuilder getContactNumbers() {
+        return contactNumbers;
+    }
 }
